f6b70baa42fac6b7974897921316aa0679865e84
ESPboy <ESPboy.edu@gmail.com>
2020-05-01 21:13:52 +0300

    updating games

---
+++ games_h/game.h
@@ -0,0 +1,25 @@
+static const unsigned char gameControl[] PROGMEM = {'9','8','6','7','9','0','S','1'};
+//UP,DOWN,LEFT,RIGHT,ACT,ESC,LFT,RGT
+
+static const unsigned char gameControlType[] PROGMEM = {3};
+/*
+if gameControl[0] == 0
+then using gameControlTypes
+0 - ?
+1  K_O, K_P, K_Q, K_A, K_SPACE, K_M, K_0, K_1,
+2  K_Z, K_X, K_Q, K_A, K_SPACE, K_ENTER, K_0, K_1,
+3  K_6, K_7, K_9, K_8, K_0, K_ENTER, K_SPACE, K_1,
+4  K_5, K_8, K_8, K_7, K_0, K_ENTER, K_SPACE, K_1
+*/
+
+static const unsigned char gameScreenExist = 1; 
+
+static const unsigned char gameScreen[] PROGMEM = {
+
+};
+
+
+
+static const unsigned char gameItself[] PROGMEM = {
+
+};
